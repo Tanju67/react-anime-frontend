bca34aad17cbcff2a12d1b5f43e18f28d194bff2
Tanju Özer <tanjuozer64@gmail.com>
2024-04-13 18:19:50 +0200

    make character detail page responsive

--- src/components/character/CharacterDetail.jsx
+++ src/components/character/CharacterDetail.jsx
@@ -1,16 +1,12 @@
-import React from "react";
-import styles from "./CharacterDetail.module.css";
+/* eslint-disable react/prop-types */
 import { Link, useParams } from "react-router-dom";
+import styles from "./CharacterDetail.module.css";
 import Button from "../../shared/UIElements/Button";
+import { formatDate } from "../../shared/utils/helper";
 
 function CharacterDetail({ data, actor }) {
-  console.log(data);
   const id = useParams().id;
-  const formattedDate = new Date(data.birthday).toLocaleDateString("en-EN", {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  });
+
   return (
     <div className={styles.page}>
       <div className={styles.subNav}>
@@ -21,23 +17,31 @@ function CharacterDetail({ data, actor }) {
           <img src={data.images.jpg.image_url} alt={data.name} />
         </div>
         <div className={styles.infoBox}>
-          <h2>{data.name}</h2>
-          {actor && (
-            <p className={styles.birth}>
-              <span>Birthday:</span>
-              <span>{formattedDate}</span>
-            </p>
-          )}
-          {!actor && data?.nicknames?.length > 0 && (
-            <p className={styles.nick}>
-              <span>Nicknames:</span>{" "}
-              <ul>
-                {data?.nicknames?.map((item, i) => (
-                  <li key={i}>{item},</li>
-                ))}
-              </ul>
-            </p>
-          )}
+          <div className={styles.flexBox}>
+            <div className={styles.imgBoxSmall}>
+              <img src={data.images.jpg.image_url} alt={data.name} />
+            </div>
+            <div>
+              <h2>{data.name}</h2>
+              {actor && (
+                <p className={styles.birth}>
+                  <span>Birthday:</span>
+                  <span>{formatDate(data.birthday)}</span>
+                </p>
+              )}
+              {!actor && data?.nicknames?.length > 0 && (
+                <p className={styles.nick}>
+                  <span>Nicknames:</span>{" "}
+                  <ul>
+                    {data?.nicknames?.map((item, i) => (
+                      <li key={i}>{item},</li>
+                    ))}
+                  </ul>
+                </p>
+              )}
+            </div>
+          </div>
+
           <p className={styles.about}>{data.about}</p>
           {actor && data?.website_url?.length > 0 && (
             <Button to={data.website_url} size={"sm"}>
